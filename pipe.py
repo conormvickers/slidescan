f96aeb63dd5ef50889c802658e304d4d349b4d11
conormvickers <conormvickers@gmail.com>
2024-11-18 19:26:40 -0500

    ok now working?

--- pipe.py
+++ pipe.py
@@ -17,46 +17,43 @@ playpause = "https://homeassistant.docdrive.link/api/webhook/YX7ptzJCNDcuvtfvmrx
 volumeup = "https://homeassistant.docdrive.link/api/webhook/ccHiaOkjUEi2qqT4oXmAic66tjGW2GwH"
 volumedown = "https://homeassistant.docdrive.link/api/webhook/q0oX9549tRvZcvkV4Lq1Ri4CI96I7yrh"
 url = playpause
-lastgesture = ''
 
-def rate_limited_function():
+def rate_limited_function(url):
     # Your function code here
-    print("Function fired")
+    print("Function fired ", url)
     
-    match lastgesture:
-        case 'Victory':
-            url = playpause
-        case 'Thumb_Up':
-            url = volumeup
-        case 'Thumb_Down':
-            url = volumedown
-        case _:
-            url = ''
-            return
+    
 
     response = requests.get(url)
 
     print(response.text)
     
     
-def fire_rate_limited_function():
+def fire_rate_limited_function(gesture):
     global last_fire_time
+    
+    
+    
     current_time = time.time()  
     if current_time - last_fire_time >= 1:
-        rate_limited_function()
+        
+        url = ''
+        match gesture:
+            case 'Victory':
+                url = playpause
+            case 'Thumb_Up':
+                url = volumeup
+            case 'Thumb_Down':
+                url = volumedown
+            case _:
+                url = ''
+                return
+        
+        rate_limited_function(url)
         last_fire_time = current_time
+    else:
+        print("Throttled")
 
-# Create a gesture recognizer instance with the live stream mode:
-def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
-    global lastgesture
-    # print('gesture recognition result: {}'.format(result))
-    if result.gestures:
-        try:
-            print(result.gestures[0][0].category_name)
-            lastgesture = result.gestures[0][0].category_name
-            fire_rate_limited_function()
-        except Exception as e:
-            print(e)
 
 cap = cv2.VideoCapture('http://picam.local:8000/stream.mjpg')
 
@@ -90,8 +87,10 @@ with GestureRecognizer.create_from_options(options) as recognizer:
             mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_srgb)
             # recognizer.recognize_async(mp_image, int(time.time() * 1000))
             result =  recognizer.recognize(mp_image)
-            print(result)
-            cv2.imshow('Video', frame)
+            if result.gestures:
+                gesture = result.gestures[0][0].category_name
+                fire_rate_limited_function(gesture=gesture)
+            # cv2.imshow('Video', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         except Exception as e:
