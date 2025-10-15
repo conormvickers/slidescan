07326fba3c2bd943aef3e60d1e93fb47f49a564c
conormvickers <conormvickers@gmail.com>
2025-10-15 19:28:20 -0400

    added 3 images, letus go

--- myown.py
+++ myown.py
@@ -33,56 +33,64 @@ def add_to_canvas(canvas, img_to_add, guess_coords, debug):
         dst_pts = np.float32([keypoints2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
         H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
         matchesMask = mask.flatten().tolist()
-    else:
-        raise Exception("Not enough matches")
 
-    
+        ones = np.ones((img_to_add.shape[0], img_to_add.shape[1]), dtype=np.uint8) * 255
+
+        warped_image = cv2.warpPerspective(img_to_add, H, (img_to_add.shape[1]*2, img_to_add.shape[0]*2, ))
+        warped_mask = cv2.warpPerspective(ones, H, (img_to_add.shape[1]*2, img_to_add.shape[0]*2, ))
+
+        print("matches found")
+        print(datetime.now() - start_time)
+
         
-    ones = np.ones((img_to_add.shape[0], img_to_add.shape[1]), dtype=np.uint8) * 255
+        toreplace = canvas[guess_coords[1]:guess_coords[3] *2, guess_coords[0]:guess_coords[2] * 2, :]
 
-    warped_image = cv2.warpPerspective(img_to_add, H, (img_to_add.shape[1]*2, img_to_add.shape[0]*2, ))
-    warped_mask = cv2.warpPerspective(ones, H, (img_to_add.shape[1]*2, img_to_add.shape[0]*2, ))
+        
+        if debug:
+            matchesimage = cv2.drawMatches(img_to_add, keypoints1, guess, keypoints2, good_matches, None)
+            cv2.imshow('Matches', cv2.resize(matchesimage, (800, 600)))
+            cv2.imshow('warped_image', cv2.resize( warped_image, (800, 600)))
+            cv2.imshow('warped_mask', cv2.resize( warped_mask, (800, 600)))
+            cv2.imshow('canvas', cv2.resize( canvas, (800, 600)))
+            cv2.imshow('guess', cv2.resize( guess, (800, 600)))
+            cv2.imshow('img_to_add', cv2.resize( toreplace, (800, 600)))
+            cv2.waitKey(0)
 
-    print("matches found")
-    print(datetime.now() - start_time)
-    # Draw the matches
-    if debug:
-        img3 = cv2.drawMatches(img_to_add, keypoints1, guess, keypoints2, good_matches, None)
-        cv2.imshow('Matches', cv2.resize(img3, (800, 600)))
-        cv2.imshow('warped_image', cv2.resize( warped_image, (800, 600)))
-        cv2.imshow('warped_mask', cv2.resize( warped_mask, (800, 600)))
-        cv2.waitKey(0)
+        toreplace[np.where(warped_mask > 1)] = warped_image[np.where(warped_mask > 1)]
 
-    # for i in range(0, canvas.shape[0]):
-    #     for j in range(0, canvas.shape[1]):
-    #         if np.any(warped_image[i,j,:] != 0):
-    #             canvas[i,j,:] = warped_image[i,j,:]
+        canvas[guess_coords[1]:guess_coords[3] *2, guess_coords[0]:guess_coords[2] * 2, :] = toreplace
 
-    
+        print("canvas updated")
+        print(datetime.now() - start_time)
 
-    # canvas = cv2.bitwise_and(canvas, warped_image, mask=warped_mask)
-    canvas[np.where(warped_mask > 100)] = warped_image[np.where(warped_mask > 100)]
+        if debug:
+            cv2.imshow('canvas', cv2.resize( canvas, (800, 600)))
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
+        return canvas
 
-    print("canvas updated")
-    print(datetime.now() - start_time)
-    # Display the blended image
-    if debug:
-        cv2.imshow('canvas', cv2.resize( canvas, (800, 600)))
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-    return canvas
+    else:
+         canvas[guess_coords[1]:guess_coords[3], guess_coords[0]:guess_coords[2], :] = img_to_add
+         print("not enough matches found - fallback to guess")
+         return canvas
 
 
 
-img2 = cv2.imread('./images/level0/1-0.jpg')
+
+img2 = cv2.imread('./images/level0/2-0.jpg')
 canvas = np.zeros((img2.shape[0] * 2, img2.shape[1] * 2, 3), dtype=np.uint8) 
 canvas[0:img2.shape[0] , 0:img2.shape[1], :] = img2
 
-guess_coords = (0,0, img2.shape[1], img2.shape[0])
-img1 = cv2.imread('./images/level0/0-0.jpg')
+guess_coords = (0, int(img2.shape[0]*0.5), img2.shape[1] , int(img2.shape[0] * 1.5))
+img1 = cv2.imread('./images/level0/1-0.jpg')
+
+
+
 start_time = datetime.now()
-returned = add_to_canvas(canvas, img1, guess_coords, True)
-# cv2.imshow('result.png', returned)
-# cv2.waitKey(0)
+returned = add_to_canvas(canvas, img1, guess_coords, True )
+
+img3 = cv2.imread('./images/level0/0-0.jpg')
+returned = add_to_canvas(returned, img3, (0, img2.shape[0], img2.shape[1], img2.shape[0]*2), False)
+cv2.imshow('result.png', cv2.resize( returned, (800, 600))) # (img_to_add, returned)
+cv2.waitKey(0)
